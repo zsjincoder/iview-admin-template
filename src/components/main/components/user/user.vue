091ea1c3a7037eec014124b5f1ed909cc904dec5
zsjin <1932019050@qq.com>
2020-09-15 15:03:07 +0800

    ddd

---
+++ src/components/main/components/user/user.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="user-avatar-dropdown">
+    <Dropdown @on-click="handleClick">
+      <Badge :dot="!!messageUnreadCount">
+        <Avatar :src="userAvatar"/>
+      </Badge>
+      <Icon :size="18" type="md-arrow-dropdown"></Icon>
+      <DropdownMenu slot="list">
+        <DropdownItem name="message">
+          消息中心<Badge style="margin-left: 10px" :count="messageUnreadCount"></Badge>
+        </DropdownItem>
+        <DropdownItem name="logout">退出登录</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  </div>
+</template>
+
+<script>
+import './user.less'
+import { mapActions } from 'vuex'
+export default {
+  name: 'User',
+  props: {
+    userAvatar: {
+      type: String,
+      default: ''
+    },
+    messageUnreadCount: {
+      type: Number,
+      default: 20
+    }
+  },
+  methods: {
+    ...mapActions([
+      'handleLogOut'
+    ]),
+    logout () {
+      this.handleLogOut().then(() => {
+        this.$router.push({
+          name: 'login'
+        })
+      })
+    },
+    message () {
+      console.log('打开消息盒子')
+    },
+    handleClick (name) {
+      switch (name) {
+        case 'logout': this.logout()
+          break
+        case 'message': this.message()
+          break
+      }
+    }
+  }
+}
+</script>
---
+++ src/store/module/user.js
@@ -0,0 +1,10 @@
+export default {
+  state: {
+    access: '',
+    unreadCount: 2,
+    avatarImgPath: 'https://dss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2156833431,1671740038&fm=26&gp=0.jpg'
+  },
+  getters: {
+    getAccess: (state) => state.access
+  }
+}
